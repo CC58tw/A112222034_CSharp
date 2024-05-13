6516d0d9a1279c4f5d72f8e36d0390754a9bdb3b
CC58tw <169029981+CC58tw@users.noreply.github.com>
2024-05-13 18:06:53 +0800

    113-05-13

--- Ch6 Tutorial/Program6_6/chp6_prob6/Form1.cs
+++ Ch6 Tutorial/Program6_6/chp6_prob6/Form1.cs
@@ -11,22 +11,82 @@ namespace chp6_prob6
 {
     public partial class Form1 : Form
     {
-        const decimal stayPerDay = 550.00m;
+        const decimal stayPerDay = 3500.00m;
         public Form1()
         {
             InitializeComponent();
         }
 
-       
+        private decimal CalcStayCharges(int day)
+        {
+            return stayPerDay * day;
+        }
+        private decimal CalcMiscCharges(decimal foodBill, decimal spaBill, decimal carRental, decimal medicalBill)
+        {
+            return foodBill + spaBill + carRental + medicalBill;
+        }
+        private decimal CalcTotalCharges(decimal stayBill, decimal miscellaneous)
+        {
+            return stayBill + miscellaneous;
+        }
+
+        private bool InputIsValid(ref int days, ref decimal fb, ref decimal sb, ref decimal cb, ref decimal mb)
+        {
+            if (int.TryParse(textBox1.Text, out days))
+            {
+                if (decimal.TryParse(textBox2.Text, out fb))
+                {
+                    if (decimal.TryParse(textBox3.Text, out sb))
+                    {
+                        if (decimal.TryParse(textBox4.Text, out cb))
+                        {
+                            if (decimal.TryParse(textBox5.Text, out mb))
+                                return true;
+                            else
+                            {
+                                MessageBox.Show("其他娛樂費用 格式錯誤");
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("租車費用 格式錯誤");
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("SPA&按摩費用 格式錯誤");
+                        return false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("用餐費用 格式錯誤");
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("天數 格式錯誤");
+                return false;
+            }
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int days;
-            decimal fb, sb, cb, mb;
+            int days = 0;
+            decimal fb = 0m, sb = 0m, cb = 0m, mb = 0m;
             decimal stayCharge, miscCharge;
             decimal totalCharge;
-         
-
+            
+            if (InputIsValid(ref days, ref fb, ref sb, ref cb, ref mb))
+            {
+                stayCharge = CalcStayCharges(days);
+                miscCharge = CalcMiscCharges(fb, sb, cb, mb);
+                totalCharge = CalcTotalCharges(stayCharge, miscCharge);
+                MessageBox.Show($"總共花費 {totalCharge} 元");
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
--- Ch6 Tutorial/Tutorial 6-4/Cups To Ounces/Cups To Ounces/Form1.cs
+++ Ch6 Tutorial/Tutorial 6-4/Cups To Ounces/Cups To Ounces/Form1.cs
@@ -20,13 +20,34 @@ namespace Cups_To_Ounces
         // The CupsToOunces method accepts a number
         // of cups as an argument and returns the
         // equivalent number of fluid ounces.
-     
+
+
+        private double CupsToOunces(double cups)
+        { return cups * 8.0; }
+        private void CupsToOunces2(double cups, out double ounces)
+        { ounces = cups * 8.0; }
 
         private void convertButton_Click(object sender, EventArgs e)
         {
-          
-        }
+            // Variables to hold cups and ounces
+            double cups, ounces;
 
+            // Get the number of cups.
+            if (double.TryParse(cupsTextBox.Text, out cups))
+            {
+                // Convert the cups to ounces.
+                //ounces = CupsToOunces(cups);
+                CupsToOunces2(cups, out ounces);
+
+                // Display the ounces.
+                ouncesLabel.Text = ounces.ToString("n1");
+            }
+            else
+            {
+                // Display an error message.
+                MessageBox.Show("Enter a valid number");
+            }
+        }
         private void exitButton_Click(object sender, EventArgs e)
         {
             // Close the form.

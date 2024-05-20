42ba552d9822c219e2baf1a3765aef0e4704cd00
CC58tw <a112222034@mail.shu.edu.tw>
2024-05-20 16:45:13 +0800

    113-05-20

---
+++ Ch7 Tutorial/Tutorial 7-1/Lottery Numbers/Lottery Numbers/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery_Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            int[] LotteryNumbers = new int[5];
+            Random rand = new Random();
+            Label[] LabelArray = { firstLabel, secondLabel, thirdLabel, fourthLabel, fifthLabel };
+
+            for (int i = 0; i < LotteryNumbers.Length; i++)
+            {
+                LotteryNumbers[i] = rand.Next(42) + 1;
+                LabelArray[i].Text = LotteryNumbers[i].ToString();
+            }
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
